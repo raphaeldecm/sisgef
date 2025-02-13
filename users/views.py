cc828ba67caab14a85f946c412b7789397ffbe83
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2025-02-12 22:35:24 -0300

    Adicionar sistema de autenticação com modelo de usuário personalizado e formulários de login e cadastro

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import EmailAuthenticationForm, CustomUserCreationForm
 
 def home(request):
   return render(request, 'users/home.html')
@@ -12,15 +13,14 @@ def autenticated(request):
 
 def login_view(request):
   if request.method == 'POST':
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
+    form = EmailAuthenticationForm(request, request.POST)
+    if form.is_valid():
+      user = form.get_user()
       login(request, user)
       return redirect('autenticated')
-    else:
-      return render(request, 'users/login.html', {'error': 'Invalid username and password'})
-  return render(request, 'users/login.html')
+  else:
+    form = EmailAuthenticationForm()
+  return render(request, 'users/login.html', {'form': form})
 
 def logout_view(request):
   logout(request)
@@ -28,7 +28,7 @@ def logout_view(request):
 
 def signup_view(request):
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = CustomUserCreationForm(request.POST)
     if form.is_valid():
       user = form.save()
       login(request, user)
@@ -36,5 +36,5 @@ def signup_view(request):
     else:
       return render(request, 'users/signup.html', {'form': form})
   else:
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     return render(request, 'users/signup.html', {'form': form})
