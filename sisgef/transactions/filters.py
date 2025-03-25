6d834ccd7a7850b3988d2ce2e8fab660227bc1ae
Raphael Muniz <raphael@copin.ufcg.edu.br>
2025-03-25 15:28:51 -0300

    Adicionar filtros de valor e data na lista de despesas, além de melhorias na exibição de categorias e ajustes no CSS

--- sisgef/transactions/filters.py
+++ sisgef/transactions/filters.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from decimal import InvalidOperation
+
 from django_filters import rest_framework as django_filters
 
 from sisgef.transactions.models import Category
@@ -18,17 +21,37 @@ class CategoryFilter(django_filters.FilterSet):
 
 class ExpenseFilter(django_filters.FilterSet):
     description = django_filters.CharFilter(lookup_expr="icontains", label="Descrição")
-    date = django_filters.DateFromToRangeFilter(label="Data")
+    date_from = django_filters.DateFilter(
+        field_name="date", lookup_expr="gte", label="De")
+    date_to = django_filters.DateFilter(
+        field_name="date", lookup_expr="lte", label="Até")
     category = django_filters.ModelChoiceFilter(
         queryset=Category.objects.filter(type=Category.Type.EXPENSE),
         label="Categoria",
     )
-    value = django_filters.NumericRangeFilter(label="Valor")
+    value_from = django_filters.CharFilter(
+        field_name="value", method="filter_value_from", label="De")
+    value_to = django_filters.CharFilter(
+        field_name="value", method="filter_value_to", label="Até")
     status = django_filters.ChoiceFilter(
         choices=Expense.PaymentStatus.choices,
         label="Situação",
     )
 
+    def filter_value_from(self, queryset, name, value):
+        try:
+            clean_value = Decimal(value.replace(",", "."))  # Converte "1.234,56" para Decimal
+            return queryset.filter(**{f"{name}__gte": clean_value})
+        except (ValueError, InvalidOperation):
+            return queryset  # Se a conversão falhar, retorna o queryset original
+
+    def filter_value_to(self, queryset, name, value):
+        try:
+            clean_value = Decimal(value.replace(",", "."))
+            return queryset.filter(**{f"{name}__lte": clean_value})
+        except (ValueError, InvalidOperation):
+            return queryset
+
     class Meta:
         model = Expense
-        fields = ["description", "date", "category", "value", "status"]
+        fields = ["description", "date", "category", "status"]
