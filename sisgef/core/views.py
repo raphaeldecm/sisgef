73dae487fb866cd5f67113c86bafe9468bfbe99e
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2025-05-15 18:37:37 -0300

    Adicionar mixin de autenticação ao DashboardView, atualizar estilos de botão e classes CSS, e remover logo não utilizado

--- sisgef/core/views.py
+++ sisgef/core/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 from .mixins import TitleViewMixin
 
 
-class DashboardView(TitleViewMixin, TemplateView):
+class DashboardView(LoginRequiredMixin, TitleViewMixin, TemplateView):
     template_name = "core/dashboard.html"
     title = "Dashboard"
     subtitle = "Bem-vindo ao Sistema de Gestão Financeira"
