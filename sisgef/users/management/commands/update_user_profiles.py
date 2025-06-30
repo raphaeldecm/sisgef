186dd421505817b50f49e567d6bea1e433afe3c0
Pablo Muris <PabloMuris@protonmail.com>
2025-06-30 11:34:13 -0300

    initiaL CHANGE

--- sisgef/users/management/commands/update_user_profiles.py
+++ sisgef/users/management/commands/update_user_profiles.py
@@ -1,18 +1,33 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group,Permission
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext_lazy as _
 
 
 class Command(BaseCommand):
     help = """
-        Create django groups to represent user profiles,
-        based in models using decorator 'profile'
+        Cria grupos de perfil de usuário: Gestor e Secretário,
+        e define as permissões associadas.
     """
 
     def handle(self, *args, **options):
-        profile_groups = set()
-        profile_groups.add("Gerente")
-        profile_groups.add("Colaborador")
-        for group_name in profile_groups:
+   
+        perfil_grupos = {
+            "Gestor": Permission.objects.all(),  
+            "Secretário": Permission.objects.filter(
+                codename__in=[
+                    # Receitas
+                    "add_income", "change_income", "view_income",
+                    # Despesas
+                    "add_expense", "change_expense", "view_expense",
+                    # Categorias
+                    "add_category", "change_category", "delete_category", "view_category",
+                ]
+            ),
+        }
+
+        for group_name, permissoes in perfil_grupos.items():
             group, created = Group.objects.get_or_create(name=_(group_name))
-        self.stdout.write("Profile groups were updated successfully")
+            group.permissions.set(permissoes)
+            self.stdout.write(f"Grupo '{group_name}' atualizado com {'criação' if created else 'sucesso'}.")
+
+        self.stdout.write(self.style.SUCCESS("Grupos de perfil e permissões configurados com sucesso."))
